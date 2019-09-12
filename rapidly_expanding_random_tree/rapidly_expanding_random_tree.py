19d73f332d01aef52921934f121ca7eefeca604c
senthil <senthillihtnes1994@gmail.com>
2019-09-12 18:17:41 -0500

    task1 of rrt_tree
    -- A rrt tree can be generated on a plain canvas
    -- Parameters of the algo can be configured.

---
+++ rapidly_expanding_random_tree/rapidly_expanding_random_tree.py
@@ -0,0 +1,83 @@
+import numpy as np
+np.random.seed(5000)
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib import colors as mcolors
+
+
+
+class rrt_node:
+  def __init__(self, position):
+    self.position = position
+    self.all_children = []
+  def set_parent(self, parent):
+    self.parent =  parent
+  def add_child(self, child):
+    self.all_children.append(child)
+
+
+def generate_random_vector(dimensions=2, max_values=[100,100]):
+  random_vector = np.random.uniform(size=dimensions) * np.array(max_values)
+  return random_vector
+
+def find_the_nearest_node(nodes_list, target_node):
+  best_distance = float("inf")
+  nearest_node = None
+
+  for node in nodes_list:
+    distance = np.linalg.norm(node.position-target_node.position)
+    # print distance
+    if distance < best_distance:
+      best_distance = distance
+      nearest_node = node
+  return nearest_node
+
+def plot_rrt_tree(nodes_list, max_values=[100,100]):
+  'Writren by referrring Matplot lib documentation'
+
+  all_lines = []
+  for node in nodes_list:
+    for child in node.all_children:
+      all_lines.append([node.position, child.position])
+
+  lines = np.array(all_lines)
+  # print 'total_lines', lines.shape
+  fig, ax = plt.subplots()
+  ax.set_xlim(0, max_values[0])
+  ax.set_ylim(0, max_values[1])
+  line_segments = LineCollection(lines, linewidths=(0.5, 1, 1.5, 2), linestyle='solid')
+  ax.add_collection(line_segments)
+  ax.set_title('rapidly expanding random tree')
+  plt.show()
+  
+
+def build_expanding_rrt(qinit=[50.0,50.0], vertex_count=10000, incremental_distance=1,
+                        planning_domain=2):
+  first_node = rrt_node(np.array(qinit))
+  first_node.parent = None
+  all_nodes = [first_node]
+
+  for i in range(vertex_count):
+    random_position = generate_random_vector(planning_domain)
+    new_node = rrt_node(random_position) 
+    nearest_node = find_the_nearest_node(all_nodes, new_node)
+    unit_distance_position = (new_node.position - nearest_node.position) / np.linalg.norm(new_node.position - nearest_node.position) * incremental_distance + nearest_node.position
+    new_node.position = unit_distance_position
+    new_node.set_parent(nearest_node)
+    nearest_node.add_child(new_node)
+
+    #print nearest_node.position, new_node.position, np.linalg.norm(nearest_node.position-new_node.position), random_position
+    all_nodes.append(new_node)
+  plot_rrt_tree(all_nodes)
+  
+
+  
+
+if __name__=='__main__':
+  build_expanding_rrt()
+
+
+  
+
