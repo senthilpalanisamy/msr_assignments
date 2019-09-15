1f558587efa4a943c180e5cb42930a658cbb25b4
senthil <senthillihtnes1994@gmail.com>
2019-09-14 22:03:12 -0500

    Task 2 - rrt generation with circles
    - Algo for finding path from start to destination not yet developed

---
+++ rapidly_expanding_random_tree/collision_generation.py
@@ -0,0 +1,78 @@
+from algo_constants import *
+import numpy as np
+np.random.seed(RANDOM_SEED)
+
+class circle:
+  def __init__(self, center, radius):
+    self.center = np.array(center)
+    self.radius = radius
+
+class canvas_generator_2d_circular_obstacles:
+  dimensions = NUM_DIMENSIONS
+  canvas_x_start = X_START 
+  canvas_x_end = X_END
+  canvas_y_start = Y_START
+  canvas_y_end = Y_END
+
+  def __init__(self, no_of_circles, max_radii, min_radii):
+    self.no_of_circles = no_of_circles
+    self.max_radii = max_radii
+    self.min_radii = min_radii
+    self.all_circles = []
+
+  def genrate_canvas(self):
+    
+      for circle_index in range(self.no_of_circles):
+        center = np.random.uniform(size=self.dimensions) * np.array([X_END, Y_END])
+        radius = self.min_radii + np.random.uniform(size=1) * (self.max_radii - self.min_radii)
+        new_circle = circle(center, radius)
+        self.all_circles.append(new_circle)
+
+  def does_line_intersect_circle(self, line):
+    '''
+     u value is the scalar mulitple of P2-P1 at which the shortest distance
+     between the point and line lies
+    '''
+    (x1, y1), (x2,y2) = line
+    minimum_line_length = 1e-6
+
+    for each_circle in self.all_circles:
+      xc, yc = each_circle.center
+
+      distance_p1_to_p2 = np.linalg.norm(line[0] - line[1])
+
+      if(distance_p1_to_p2 > minimum_line_length):
+        u_value = ((xc - x1) * (x2 - x1) + (yc - y1) * (y2 - y1)) / (distance_p1_to_p2 ** 2)
+        shortest_distance_point = np.array([x1 + u_value * (x2-x1), y1 + u_value * (y2 - y1)])
+        shortest_distance = np.linalg.norm(each_circle.center - shortest_distance_point)
+        if(shortest_distance > each_circle.radius):
+          continue
+        elif(0<= u_value <=1):
+          return True
+        elif(np.linalg.norm(line[1] - each_circle.center) <= each_circle.radius or
+             np.linalg.norm(line[0] - each_circle.center) <= each_circle.radius):
+          return True
+          
+      else:
+        print 'points P1=', line[0], 'P2=', line[1], 'are coincident and' + \
+                                                     'do\'t form a line'
+        raise 
+    return False
+      
+      
+
+if __name__ =='__main__':
+  canvas_2d = canvas_generator_2d_circular_obstacles(no_of_circles=10, 
+                                                     max_radii=10, min_radii=2)
+  canvas_2d.genrate_canvas()
+  line = np.array([[50,50], [60,60]])
+  canvas_2d.does_line_intersect_circle(line)
+  print canvas_2d 
+
+    
+
+
+
+  
+
+  
--- rapidly_expanding_random_tree/rapidly_expanding_random_tree.py
+++ rapidly_expanding_random_tree/rapidly_expanding_random_tree.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 from matplotlib import colors as mcolors
 
-from algo_constants import *
+from collision_generation import *
 
 np.random.seed(RANDOM_SEED)
 
@@ -37,7 +37,7 @@ def find_the_nearest_node(nodes_list, target_node):
       nearest_node = node
   return nearest_node
 
-def plot_rrt_tree(nodes_list, max_values=[X_END, Y_END]):
+def plot_rrt_tree(nodes_list, canvas_2d, max_values=[X_END, Y_END]):
   'Writren by referrring Matplot lib documentation'
 
   all_lines = []
@@ -50,17 +50,35 @@ def plot_rrt_tree(nodes_list, max_values=[X_END, Y_END]):
   fig, ax = plt.subplots()
   ax.set_xlim(0, max_values[0])
   ax.set_ylim(0, max_values[1])
+
+  centers = [circle.center for circle in canvas_2d.all_circles]
+  radii = [circle.radius for circle in canvas_2d.all_circles]
+
+  patches = [plt.Circle(center, radius) for center, radius in zip(centers, radii)]
+  coll = matplotlib.collections.PatchCollection(patches, facecolors='black')
+
+
   line_segments = LineCollection(lines, linewidths=(0.5, 1, 1.5, 2), linestyle='solid')
+  #matplotlib.patches.circle()
+  ax.add_collection(coll)
   ax.add_collection(line_segments)
+
+
   ax.set_title('rapidly expanding random tree')
+
   plt.show()
   
 
-def build_expanding_rrt(qinit=[50.0,50.0], vertex_count=1000, incremental_distance=1,
+def build_expanding_rrt(qinit=[80.0,80.0], vertex_count=5000, incremental_distance=1,
                         planning_domain=NUM_DIMENSIONS):
   first_node = rrt_node(np.array(qinit))
   first_node.parent = None
   all_nodes = [first_node]
+
+  canvas_2d = canvas_generator_2d_circular_obstacles(no_of_circles=10, 
+                                                       max_radii=10, min_radii=2)
+  canvas_2d.genrate_canvas()
+
 
   for i in range(vertex_count):
     random_position = generate_random_vector(planning_domain)
@@ -68,12 +86,17 @@ def build_expanding_rrt(qinit=[50.0,50.0], vertex_count=1000, incremental_distan
     nearest_node = find_the_nearest_node(all_nodes, new_node)
     unit_distance_position = (new_node.position - nearest_node.position) / np.linalg.norm(new_node.position - nearest_node.position) * incremental_distance + nearest_node.position
     new_node.position = unit_distance_position
-    new_node.set_parent(nearest_node)
-    nearest_node.add_child(new_node)
+    line = np.array([nearest_node.position, new_node.position])
+    is_intersect = canvas_2d.does_line_intersect_circle(line)
 
-    #print nearest_node.position, new_node.position, np.linalg.norm(nearest_node.position-new_node.position), random_position
-    all_nodes.append(new_node)
-  plot_rrt_tree(all_nodes)
+
+
+    if(not is_intersect):
+      new_node.set_parent(nearest_node)
+      nearest_node.add_child(new_node)
+      all_nodes.append(new_node)
+
+  plot_rrt_tree(all_nodes, canvas_2d)
   
 
   
